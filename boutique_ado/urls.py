ab201b5739fc437079fa505adbd92b30db11fa44
AMartins87 <aneta.martins@outlook.com>
2022-08-26 18:19:35 +0000

    Add 404 handler, views and html

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
+
+# Handler path to handler views
+handler404 = 'boutique_ado.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
